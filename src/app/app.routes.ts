52216af3d41f784337e25af759c69c6e9ea9f7c1
Pablorico98 <pablo.i.rico98@gmial.com>
2026-04-14 19:28:34 -0300

    Entrega TP1: Ruteo, Lazy Loading y Diseño

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Ruta por defecto (cuando entras a localhost:4200 sin nada más)
+  { 
+    path: '', 
+    loadComponent: () => import('./bienvenida/bienvenida').then(m => m.Bienvenida) 
+  },
+  { 
+    path: 'login', 
+    loadComponent: () => import('./login/login').then(m => m.Login) 
+  },
+  { 
+    path: 'registro', 
+    loadComponent: () => import('./registro/registro').then(m => m.Registro) 
+  },
+  { 
+    path: 'sobre-mi', 
+    loadComponent: () => import('./sobre-mi/sobre-mi').then(m => m.SobreMi) 
+  },
+  // Ruta comodín (Wildcard) para atrapar cualquier error
+  { 
+    path: '**', 
+    loadComponent: () => import('./error/error').then(m => m.Error) 
+  }
+];
