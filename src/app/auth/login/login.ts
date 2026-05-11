88c321fb88aa402d9b096ef7c7284a5937ca6e78
Pablorico98 <pablo.i.rico98@gmail.com>
2026-05-11 16:21:23 -0300

    refactor(login): implementar estado de carga y validación de credenciales con feedback visual

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject,signal } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from '../auth'; 
 import { Router , RouterLink} from '@angular/router';
@@ -14,6 +14,9 @@ export class Login {
   private fb = inject(FormBuilder);
   private router = inject(Router);
   private authService = inject(AuthService);
+//Signals para controlar la interfaz
+  cargando = signal(false);
+  errorLogin = signal<string | null>(null);
 
   formLogin: FormGroup = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
@@ -22,11 +25,17 @@ export class Login {
 
   async entrar() {
     if (this.formLogin.valid) {
+      // 1. Bloqueamos el botón y limpiamos errores previos
+      this.cargando.set(true);
+      this.errorLogin.set(null);
+
       const { email, password } = this.formLogin.value;
       const { data, error } = await this.authService.iniciarSesion(email, password);
 
       if (error) {
         console.log("Error: " + error.message);
+        this.errorLogin.set('Correo o contraseña incorrectos');
+        this.cargando.set(false);
       } else {
         console.log("Sesión iniciada", data);
         this.router.navigate(['/']);
@@ -35,10 +44,13 @@ export class Login {
   }
 
 completarCampos(email: string, clave: string) {
+  this.errorLogin.set(null);
   this.formLogin.patchValue({
     email: email,
     password: clave
   });
+ 
 }
+
 
 }
